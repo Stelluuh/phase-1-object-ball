f8bf081235a6c3f45553d1d861c2160d9b8cbc89
stella <sftamesis13@gmail.com>
2023-09-02 12:26:35 -0700

    complete gameObject function and homeTeamName

--- src/01-simple-debug.js
+++ src/01-simple-debug.js
@@ -1,6 +1,13 @@
-console.log('Simple debugging example running.')
-debugger
+// console.log('Simple debugging example running.')
+// debugger
 
-let x = 99
-debugger
-console.log(x)
+// let x = 99
+// debugger
+// console.log(x)
+
+function homeTeamName() {
+    let game = gameObject()
+    return game.home.homeTeamName
+}
+
+homeTeamName()
